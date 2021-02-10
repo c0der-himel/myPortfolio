098817102cb3de31c0a6cc3f45b178c9be5a31d7
himel <himel777hussain@gmail.com>
2021-02-10 22:12:22 +0600

    fixing

---
+++ js/custom.js
@@ -0,0 +1,54 @@
+const all = document.getElementById('all');
+const full = document.getElementById('full');
+const front = document.getElementById('front');
+const back = document.getElementById('back');
+const allP = document.querySelectorAll('.all');
+const fullP = document.querySelectorAll('.full');
+const frontP = document.querySelectorAll('.front');
+const backP = document.querySelectorAll('.back');
+
+all.addEventListener('click', function (e) {
+  for (let i = 0; i < allP.length; i++) {
+    allP[i].classList.remove('d-none');
+    allP[i].classList.add('d-block');
+  }
+});
+
+front.addEventListener('click', function (e) {
+  for (let i = 0; i < frontP.length; i++) {
+    frontP[i].classList.remove('d-none');
+    frontP[i].classList.add('d-block');
+  }
+  for (let i = 0; i < fullP.length; i++) {
+    fullP[i].classList.add('d-none');
+  }
+  for (let i = 0; i < backP.length; i++) {
+    backP[i].classList.add('d-none');
+  }
+});
+
+back.addEventListener('click', function (e) {
+  for (let i = 0; i < backP.length; i++) {
+    backP[i].classList.remove('d-none');
+    backP[i].classList.add('d-block');
+  }
+  for (let i = 0; i < frontP.length; i++) {
+    frontP[i].classList.add('d-none');
+  }
+  for (let i = 0; i < fullP.length; i++) {
+    fullP[i].classList.add('d-none');
+  }
+});
+
+full.addEventListener('click', function (e) {
+  for (let i = 0; i < fullP.length; i++) {
+    fullP[i].classList.remove('d-none');
+    fullP[i].classList.add('d-block');
+  }
+  for (let i = 0; i < frontP.length; i++) {
+    frontP[i].classList.add('d-none');
+  }
+  for (let i = 0; i < backP.length; i++) {
+    backP[i].classList.add('d-none');
+  }
+});
