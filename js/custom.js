4d573d3c2dbbd1c2e855bbbdd7d3556cd97c5bfe
himel <himel777hussain@gmail.com>
2021-02-10 23:12:58 +0600

    fixing

--- js/custom.js
+++ js/custom.js
@@ -10,45 +10,45 @@ const backP = document.querySelectorAll('.back');
 all.addEventListener('click', function (e) {
   for (let i = 0; i < allP.length; i++) {
     allP[i].classList.remove('d-none');
-    allP[i].classList.add('d-block');
+    allP[i].classList.add('d-block', 'animate__zoomIn');
   }
 });
 
 front.addEventListener('click', function (e) {
   for (let i = 0; i < frontP.length; i++) {
     frontP[i].classList.remove('d-none');
-    frontP[i].classList.add('d-block');
+    frontP[i].classList.add('d-block', 'animate__zoomIn');
   }
   for (let i = 0; i < fullP.length; i++) {
-    fullP[i].classList.add('d-none');
+    fullP[i].classList.add('d-none', 'animate__zoomIn');
   }
   for (let i = 0; i < backP.length; i++) {
-    backP[i].classList.add('d-none');
+    backP[i].classList.add('d-none', 'animate__zoomIn');
   }
 });
 
 back.addEventListener('click', function (e) {
   for (let i = 0; i < backP.length; i++) {
     backP[i].classList.remove('d-none');
-    backP[i].classList.add('d-block');
+    backP[i].classList.add('d-block', 'animate__zoomIn');
   }
   for (let i = 0; i < frontP.length; i++) {
-    frontP[i].classList.add('d-none');
+    frontP[i].classList.add('d-none', 'animate__zoomIn');
   }
   for (let i = 0; i < fullP.length; i++) {
-    fullP[i].classList.add('d-none');
+    fullP[i].classList.add('d-none', 'animate__zoomIn');
   }
 });
 
 full.addEventListener('click', function (e) {
   for (let i = 0; i < fullP.length; i++) {
     fullP[i].classList.remove('d-none');
-    fullP[i].classList.add('d-block');
+    fullP[i].classList.add('d-block', 'animate__zoomIn');
   }
   for (let i = 0; i < frontP.length; i++) {
-    frontP[i].classList.add('d-none');
+    frontP[i].classList.add('d-none', 'animate__zoomIn');
   }
   for (let i = 0; i < backP.length; i++) {
-    backP[i].classList.add('d-none');
+    backP[i].classList.add('d-none', 'animate__zoomIn');
   }
 });
